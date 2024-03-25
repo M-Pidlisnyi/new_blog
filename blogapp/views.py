0ff4d420b31d9b193e36bfd6571b58d2581a445e
misha <mishaprotpatriot@gmail.com>
2024-03-25 19:25:02 +0200

    add post list to category page and prof pic to about page

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Post, BlogUser
+
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
 def about(request):
-    return render(request, "about.html")
+    blog_user = BlogUser.objects.get(user=request.user)
+    return render(request, "about.html", context={"blog_user": blog_user})
 
 
 def category(request, category_id):
     current_category = Category.objects.get(id=category_id)
-    return render(request, "category.html", context={"category": current_category})
+    posts_list = Post.objects.filter(category=category_id)
+
+    return render(request, "category.html", context={"category": current_category, "posts": posts_list})
