81d5707abdc468625ee5c48039c8253c7523d792
misha <mishaprotpatriot@gmail.com>
2024-03-25 19:02:04 +0200

    changed bloguser model

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import OneToOneField
 from django.utils import timezone
 # Create your models here.
 
 
-class BlogUser(User):
-
+class BlogUser(models.Model):
+    user = OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='pics/', verbose_name="Profile Picture", null=True)
     class Meta:
         verbose_name = "Blog User"
         verbose_name_plural = "Blog Users"
