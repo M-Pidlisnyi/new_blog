a741cc7a80c5057f5b2800fb4e346f89f9fc2277
misha <mishaprotpatriot@gmail.com>
2024-03-25 15:34:41 +0200

    created basic models

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import BlogUser, Category, Post
 # Register your models here.
+admin.site.register(BlogUser)
+admin.site.register(Category)
+admin.site.register(Post)
--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+class BlogUser(User):
+
+    class Meta:
+        verbose_name = "Blog User"
+        verbose_name_plural = "Blog Users"
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True, null=False, blank=False)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+class Post(models.Model):
+    title = models.CharField(max_length=50, null=False, blank=False)
+    text = models.TextField()
+    datetime = models.DateField(null=False, blank=False, default=timezone.now)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(BlogUser, on_delete=models.CASCADE)
