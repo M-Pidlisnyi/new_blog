64376dd804347347053b8114eaaa3c8845f43173
misha <mishaprotpatriot@gmail.com>
2024-03-25 21:05:52 +0200

    added creation and deletion of posts

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -5,5 +5,6 @@ from blogapp import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('category/<int:category_id>/', views.category, name='category')
+    path('category/<int:category_id>/', views.category, name='category'),
+    path('post/delete/<int:post_id>/', views.delete_post, name='delete_post')
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Category, Post, BlogUser
-
 
 # Create your views here.
 def index(request):
@@ -15,4 +16,18 @@ def category(request, category_id):
     current_category = Category.objects.get(id=category_id)
     posts_list = Post.objects.filter(category=category_id)
 
+    if request.method == "POST":
+        new_title = request.POST.get("title")
+        new_text = request.POST.get("text", "")
+        new_image = request.POST.get("image", "")
+        blog_user = BlogUser.objects.get(user=request.user)
+        Post.objects.create(title=new_title, text=new_text, category=current_category, user=blog_user)
+        return  HttpResponseRedirect(reverse("category", args=(current_category.id,)))
+
+
     return render(request, "category.html", context={"category": current_category, "posts": posts_list})
+
+def delete_post(request, post_id):
+    deleted_post = Post.objects.get(id=post_id)
+    deleted_post.delete()
+    return HttpResponseRedirect(reverse("category", args=(deleted_post.category_id,)))
