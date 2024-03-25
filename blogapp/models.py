b38b2866da143c4df93ef5e0bca035b31e7be68a
misha <mishaprotpatriot@gmail.com>
2024-03-25 20:23:20 +0200

    displayed profile pic

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models import OneToOneField
+
 from django.utils import timezone
 # Create your models here.
 
 
 class BlogUser(models.Model):
-    user = OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_pic = models.ImageField(upload_to='pics/', verbose_name="Profile Picture", null=True)
     class Meta:
         verbose_name = "Blog User"
