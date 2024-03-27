193dc61bfaea25b5bb0a675762f8f3e347a1331a
misha <mishaprotpatriot@gmail.com>
2024-03-27 23:09:44 +0200

    added registration and authorization

--- new_blog/urls.py
+++ new_blog/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import  settings
 from django.conf.urls.static import static
+from .views import signup
 
 urlpatterns = [
     path('admin/', admin.site.urls, name="admin"),
-    path('', include('blogapp.urls'))
+    path('', include('blogapp.urls')),
+    path("accounts/", include('django.contrib.auth.urls')),
+    path("accounts/signup", signup, name="signup")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
