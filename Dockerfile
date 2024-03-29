bd436222ef2695601bf1c46e0ebe26af43724882
misha <mishaprotpatriot@gmail.com>
2024-03-29 22:56:21 +0200

    dockerized

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.11
+ENV PYTHONUNBUFFERED=1
+WORKDIR /new_blog
+COPY requirements.txt /new_blog/
+RUN pip install -r requirements.txt
+COPY . /new_blog/
+EXPOSE 8000
+ENTRYPOINT ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
--- new_blog/settings.py
+++ new_blog/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0", "192.168.1.112"]
 
 
 # Application definition
